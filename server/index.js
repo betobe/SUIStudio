7ea728548b5465cdeb0bfed71e22116d8d8eabaf
Carlos Villuendas <carlosvillu@gmail.com>
2016-05-05 12:03:06 +0200

    chore(deploy): check routes for nginx

--- server/index.js
+++ server/index.js
@@ -4,5 +4,6 @@ const log = require('debug')('suistudio:server')
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World'))
+app.get('/ping', (req, res) => res.send('Pong'))
 
 app.listen(PORT, () => log(`SUIStudio up and running in localhost:${PORT}`))
