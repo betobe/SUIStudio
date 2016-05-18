7c67b958fd93c3b21dfaa638a04bfd2b92c67e36
Carlos Villuendas <carlosvillu@gmail.com>
2016-05-18 17:00:47 +0200

    feat(workbench): dinamic list of the all components in the base dir

--- studio/client/src/components/workbench/index.js
+++ studio/client/src/components/workbench/index.js
@@ -6,13 +6,13 @@ export default class Workbench extends React.Component {
     const {category, name, component} = this.props.params
     return (
       <div className='SUIStudioWorkbench'>
-        <nav class='SUIStudioWorkbench-navigation'>
-          <ul class='SUIStudioWorkbench-navContainer'>
-            <li class='SUIStudioWorkbench-link'><Link to={`/workbench/${category}/${name}/${component}/demo`}>Demo</Link></li>
-            <li class='SUIStudioWorkbench-link'><Link to={`/workbench/${category}/${name}/${component}/documentation`}>Documentation</Link></li>
+        <nav className='SUIStudioWorkbench-navigation'>
+          <ul className='SUIStudioWorkbench-navContainer'>
+            <li className='SUIStudioWorkbench-link'><Link to={`/workbench/${category}/${name}/${component}/demo`}>Demo</Link></li>
+            <li className='SUIStudioWorkbench-link'><Link to={`/workbench/${category}/${name}/${component}/documentation`}>Documentation</Link></li>
           </ul>
         </nav>
-        <div class='SUIStudioWorkbench-content'>{this.props.children}</div>
+        <div className='SUIStudioWorkbench-content'>{this.props.children}</div>
       </div>
     )
   }
