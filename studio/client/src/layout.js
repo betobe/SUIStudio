99bbcf76ebdc8baf4bd08269cd35c0fb07f04a03
Joan Leon <joan.leon@gmail.com>
2016-05-19 12:13:48 +0200

    feat(workbench): Global readme for all studio

--- studio/client/src/layout.js
+++ studio/client/src/layout.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 import Navigation from './components/navigation'
+
+const readme = require('raw!./../../../src/REAME.md')
 
 export default class Layout extends React.Component {
   render () {
     const {children} = this.props
+    console.log(children)
     return (
       <div className='SUIStudio'>
         <Navigation />
         <div className='SUIStudio-workbench'>
-          {children !== undefined ? children : <h1>Selecciona un componente</h1>}
+          {children !== null ? children : <ReactMarkdown source={readme} />}
         </div>
       </div>
     )
   }
 }
-
