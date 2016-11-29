a80fb5519781e1a930f8bdb1d2d57d70f60215fa
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-11-29 16:51:04 +0100

    feat(META): improved general styles

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from 'react'
 import reactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import routes from './routes'
-
 import './index.scss'
 
 reactDOM.render(<Router routes={routes} history={browserHistory} />, document.getElementById('root'))
