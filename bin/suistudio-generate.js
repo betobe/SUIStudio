67758e4b5c69f1b4b76c2a217053b66cf9994d15
Carlos Villuendas <carlosvillu@gmail.com>
2017-02-28 15:55:32 +0100

    feat(cli): Generate package JSON who use babel-preset-schibsted-spain

--- bin/suistudio-generate.js
+++ bin/suistudio-generate.js
@@ -87,7 +87,7 @@ COMPONENT_PACKAGE_JSON_FILE,
   "main": "lib/index.js",
   "scripts": {
     "build": "rm -Rf ./lib && mkdir -p ./lib && npm run babel && npm run sass",
-    "babel": "../../../node_modules/.bin/babel ./src --out-dir ./lib",
+    "babel": "../../../node_modules/.bin/babel --preset schibsted-spain ./src --out-dir ./lib",
     "sass": "../../../node_modules/.bin/cpx \\"./src/**/*.scss\\" ./lib",
     "preversion": "echo \\"preversion\\"",
     "version": "npm run build",
@@ -98,16 +98,6 @@ COMPONENT_PACKAGE_JSON_FILE,
   "license": "ISC",
   "dependencies": {
     "@schibstedspain/suistudio-fatigue-deps": "github:SUI-Components/suistudio-fatigue-deps"
-  },
-  "babel": {
-    "presets": [
-      "es2015",
-      "react",
-      "stage-3"
-    ],
-    "plugins": [
-      "transform-runtime"
-    ]
   }
 }
 `
