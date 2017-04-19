38bdfb4638c5496e1ac3d0226a86ed1f1b88f475
Ignacio Tolstoy <ignacio.tolstoy@scmspain.com>
2017-04-19 11:28:47 +0200

    refactor(META): Make walk function more redeable

--- bin/walker.js
+++ bin/walker.js
@@ -2,18 +2,17 @@ const readdirSync = require('fs').readdirSync
 const statSync = require('fs').statSync
 const path = require('path')
 
-const onlyFolders = (rootPath, fileName) => {
-  return statSync(path.join(rootPath, fileName)).isDirectory()
-}
+const onlyFolders = (filePath) => statSync(filePath).isDirectory()
 
 const cwds = (baseDir) => {
   const rootDir = path.join(baseDir, 'components')
 
   return readdirSync(rootDir)
-    .filter(file => onlyFolders(rootDir, file))
-    .map(folder => readdirSync(path.join(rootDir, folder))
-      .filter(file => onlyFolders(path.join(rootDir, folder), file))
-      .map(file => path.join(rootDir, folder, file))
+    .map(file => path.join(rootDir, file))
+    .filter(onlyFolders)
+    .map(folder => readdirSync(folder)
+      .map(file => path.join(folder, file))
+      .filter(onlyFolders)
     ).reduce((x, y) => x.concat(y)) // flatten
 }
 
